44a6f7311b40ea88f379523560274366c3576c73
Caitlin <caitlinsweeneylmt@gmail.com>
2018-02-09 20:50:49 -0800

    working filter map and reduce function and tests, working on forEach

--- lab-2-sweeney/lib/fp.js
+++ lab-2-sweeney/lib/fp.js
@@ -4,50 +4,55 @@ const fp = {};
 
 
 fp.forEach = (arr, cb)=>{ 
+  
 
   for(let i = 0; i < arr.length; i++){
     let element = arr[i];
     let result = cb(element);
     arr[i] = result;
+  
   }
 
 };
 
-fp.map = (arr, cb)=>{
+// fp.map = (arr, cb)=>{
 
-  let results = [];
-  for(let i = 0; i < arr.length; i++){
-    let element = arr[i];
-    let result = cb(element);
-    results.push(result);
-  }
-  console.log("map results",results);
-  return results;
+//   let results = [];
+//   console.log("arr-map",arr);
+//   for(let i = 0; i < arr.length; i++){
+//     console.log("arr-map-for",arr);
+//     let element = arr[i];
+//     let result = cb(element);
+//     console.log("map result-for",result);
+//     results.push(result);
+//   }
+//   console.log("map results",results);
+//   return results;
 
-};
+// };
 
-fp.filter = (arr, cb)=>{
+// fp.filter = (arr, cb)=>{
 
-  let results = [];
-  for(let i = 0; i < arr.length; i++){
-    let element = arr[i];
-    let result = cb(element);
-    if(result === true){
-      console.log("element",element);
-      results.push(element);
-    }
-  }
-  return results;
+//   let results = [];
+//   for(let i = 0; i < arr.length; i++){
+//     let element = arr[i];
+//     let result = cb(element);
+//     if(result === true){
+//       console.log("element",element);
+//       results.push(element);
+//     }
+//   }
+//   return results;
 
-};
+// };
 
-fp.reduce = (arr, cb)=>{
-  let results = 0;
-  for(let i = 0; i < arr.length; i++){
-    results = cb(arr[i], results);
-  }
-  return results;
-};
+// fp.reduce = (arr, cb)=>{
+//   let results = 0;
+//   for(let i = 0; i < arr.length; i++){
+//     results = cb(arr[i], results);
+//   }
+//   return results;
+// };
 
 
 
