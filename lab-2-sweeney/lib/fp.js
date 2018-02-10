3f6f8356635d7a3c34794d3e5b271661675a13c8
Caitlin <caitlinsweeneylmt@gmail.com>
2018-02-09 20:53:04 -0800

    working map, reduce(basic), filter and forEach functions, four basic tests

--- lab-2-sweeney/lib/fp.js
+++ lab-2-sweeney/lib/fp.js
@@ -15,44 +15,44 @@ fp.forEach = (arr, cb)=>{
 
 };
 
-// fp.map = (arr, cb)=>{
+fp.map = (arr, cb)=>{
 
-//   let results = [];
-//   console.log("arr-map",arr);
-//   for(let i = 0; i < arr.length; i++){
-//     console.log("arr-map-for",arr);
-//     let element = arr[i];
-//     let result = cb(element);
-//     console.log("map result-for",result);
-//     results.push(result);
-//   }
-//   console.log("map results",results);
-//   return results;
+  let results = [];
+  console.log("arr-map",arr);
+  for(let i = 0; i < arr.length; i++){
+    console.log("arr-map-for",arr);
+    let element = arr[i];
+    let result = cb(element);
+    console.log("map result-for",result);
+    results.push(result);
+  }
+  console.log("map results",results);
+  return results;
 
-// };
+};
 
-// fp.filter = (arr, cb)=>{
+fp.filter = (arr, cb)=>{
 
-//   let results = [];
-//   for(let i = 0; i < arr.length; i++){
-//     let element = arr[i];
-//     let result = cb(element);
-//     if(result === true){
-//       console.log("element",element);
-//       results.push(element);
-//     }
-//   }
-//   return results;
+  let results = [];
+  for(let i = 0; i < arr.length; i++){
+    let element = arr[i];
+    let result = cb(element);
+    if(result === true){
+      console.log("element",element);
+      results.push(element);
+    }
+  }
+  return results;
 
-// };
+};
 
-// fp.reduce = (arr, cb)=>{
-//   let results = 0;
-//   for(let i = 0; i < arr.length; i++){
-//     results = cb(arr[i], results);
-//   }
-//   return results;
-// };
+fp.reduce = (arr, cb)=>{
+  let results = 0;
+  for(let i = 0; i < arr.length; i++){
+    results = cb(arr[i], results);
+  }
+  return results;
+};
 
 
 
--- lab-2-sweeney/test/fp.test.js
+++ lab-2-sweeney/test/fp.test.js
@@ -8,35 +8,35 @@ const isEven = (n)=>{return n % 2 === 0;};
 const answer = isEven(6);
 console.log("answer isEven", answer);
 
-// describe('given an array will reduce to a single number according to function', () => {
-//   let expected = 41;
-//   it('should return the sum of the elements[3,6,7,10,15] of the array', () => {
-//     let arr = [3,6,7,10,15];
-//     let result = fp.reduce(arr, sum);
-//     console.log("result sum reduce test",result);
-//     expect(result).toBe(expected);
-//   });
-// });
+describe('given an array will reduce to a single number according to function', () => {
+  let expected = 41;
+  it('should return the sum of the elements[3,6,7,10,15] of the array', () => {
+    let arr = [3,6,7,10,15];
+    let result = fp.reduce(arr, sum);
+    console.log("result sum reduce test",result);
+    expect(result).toBe(expected);
+  });
+});
 
-// describe('given an array will map by doubling all elements and return new array', () => {
-//   let expected = [4,6,8,10];
-//   it('should return mapped [4,6,8,10] when given [2,3,4,5]',()=>{
-//     let arr = [2,3,4,5];
-//     let result = fp.map(arr, doubler);
-//     console.log("result doubler map test", result);
-//     expect(result).toEqual(expected);
-//   });
-// });
+describe('given an array will map by doubling all elements and return new array', () => {
+  let expected = [4,6,8,10];
+  it('should return mapped [4,6,8,10] when given [2,3,4,5]',()=>{
+    let arr = [2,3,4,5];
+    let result = fp.map(arr, doubler);
+    console.log("result doubler map test", result);
+    expect(result).toEqual(expected);
+  });
+});
 
-// describe('given an array will filter and results true to isEven function',()=> {
-//   let expected = [4,10,12,36];
-//   it('should return [4,10,12,36] when given [3,4,7,10,11,12,31,36]',()=>{
-//     let arr = [3,4,7,10,11,12,31,36];
-//     let result = fp.filter(arr, isEven);
-//     expect(result).toEqual(expected);
-//   });
+describe('given an array will filter and results true to isEven function',()=> {
+  let expected = [4,10,12,36];
+  it('should return [4,10,12,36] when given [3,4,7,10,11,12,31,36]',()=>{
+    let arr = [3,4,7,10,11,12,31,36];
+    let result = fp.filter(arr, isEven);
+    expect(result).toEqual(expected);
+  });
 
-// });
+});
 
 describe('given an array will apply a doubler function to each index of an array, modifying the existing array by doubling all elements and return new array', () => {
   let expected = [4,6,8,10];
